142cbd9e9385e8dfc577c2e16c7e37735a0b37d5
Yan Yu <junhe1205@163.com>
2024-01-01 17:07:43 +0800

    创建mod

--- modmain.lua
+++ modmain.lua
@@ -1,1 +1,8 @@
-prefabe
+PrefabFiles = {
+
+}
+
+GLOBAL.setmetatale(env,{__index=function(t,k) return GLOBAL.rawget(GLOBAL,k) end})
+
+STRING.NAME._NAME = "mod物品"
+STRING.CHARACTERS.GENERIC.DESCRIBE._NAME = "你好"
