bfdaeeb1990875b4c193490cc91c445d8d811095
Yan Yu <junhe1205@163.com>
2024-01-01 18:54:30 +0800

    更新

---
+++ modinfo.lua
@@ -0,0 +1,21 @@
+name = "yy_mod"
+description = "Mod made by myself"
+author = "Yan Yu"
+version = "1.0"
+api_version = 10
+dst_compatible = true
+
+all_client_require_mod = true
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+forumthread = ""
+
+server_filter_tags = {
+
+}
+
+configuration_options ={
+    
+}
--- modmain.lua
+++ modmain.lua
@@ -6,4 +6,3 @@ GLOBAL.setmetatale(env,{__index=function(t,k) return GLOBAL.rawget(GLOBAL,k) end
 
 STRING.NAME._NAME = "mod物品"
 STRING.CHARACTERS.GENERIC.DESCRIBE._NAME = "你好"
-and
